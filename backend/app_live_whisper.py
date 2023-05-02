05a34c4c01fbd12b86664802a3ee1c6d9b706b3a
khanhdv <khanhdv@sonek.vn>
2023-05-02 20:04:07 +0700

    add Dockerfile for app_live_whipser

--- backend/app_live_whisper.py
+++ backend/app_live_whisper.py
@@ -329,5 +329,5 @@ def welcome():
 
 if __name__ == "__main__":
     # socketio.run(app, debug=True, port=5003, host="0.0.0.0")
-    app.run(debug=True, port=5003, host="0.0.0.0")
+    app.run(debug=True, port=5000, host="0.0.0.0")
 
